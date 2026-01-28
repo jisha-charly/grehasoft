73471f67c361968291933dbee1d528c0970efcf7
jisha charly <jisha.charly@gmail.com>
2026-01-28 09:43:43 +0530

    roles changed

--- frontend/src/api/services/role.service.ts
+++ frontend/src/api/services/role.service.ts
@@ -2,26 +2,20 @@ import api from "../axios";
 import type { Role } from "../../types/role";
 
 /// GET all roles
-export const getRoles = async () => {
-  const res = await api.get("/roles/");
+export const getRoles = async (): Promise<Role[]> => {
+  const res = await api.get<Role[]>("/roles/");
   return res.data;
 };
 
 // CREATE role
-export const createRole = async (data: {
-  name: string;
-  description: string;
-}) => {
- const res = await api.post(
-    "https://grehasoft-production.up.railway.app/api/roles/create/",
-    data
-  );
+export const createRole = async (
+  data: Pick<Role, "name" | "description">
+): Promise<Role> => {
+  const res = await api.post<Role>("/roles/create/", data);
   return res.data;
 };
 
 // DELETE role
-export const deleteRole = async (id: number) => {
-  const res = await api.delete(`/roles/${id}/delete/`);
-  return res.data;
+export const deleteRole = async (id: number): Promise<void> => {
+  await api.delete(`/roles/${id}/delete/`);
 };
-
