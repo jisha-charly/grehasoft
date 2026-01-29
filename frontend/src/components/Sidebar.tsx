6a40c6cc3e1661069d130ae328112bb818e96550
jisha charly <jisha.charly@gmail.com>
2026-01-29 11:26:03 +0530

    topbar and side bar changed

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -41,7 +41,7 @@ const Sidebar = () => {
       style={{ width: 260, minHeight: "100vh" }}
     >
       {/* BRAND */}
-      <div className="px-3 py-3 border-bottom border-secondary">
+      <div className="px-3 py-3  border-secondary">
         <h5 className="mb-0 fw-bold">Grehasoft</h5>
         <small className="text-secondary">Admin Panel</small>
       </div>
@@ -67,7 +67,7 @@ const Sidebar = () => {
       </div>
 
       {/* LOGOUT */}
-      <div className="px-3 py-3 border-top border-secondary">
+      <div className="px-3 py-3  border-secondary">
         <button
           onClick={handleLogout}
           className="btn btn-outline-danger w-100 d-flex align-items-center justify-content-center gap-2"
