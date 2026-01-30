d3d2fe65bf0f8e06aebdb2f633b2d02f61c30331
jisha charly <jisha.charly@gmail.com>
2026-01-30 17:36:32 +0530

    fix users backend api

--- backend/accounts/serializers.py
+++ backend/accounts/serializers.py
@@ -1,5 +1,6 @@
 import re
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+User = get_user_model()
 from rest_framework import serializers
 from .models import Client
 from .models import Project, ProjectMilestone, ProjectMember
--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -17,7 +17,8 @@ from .serializers import (
     ProjectMemberSerializer,
     DepartmentSerializer
 )
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+
 from django.contrib.auth.password_validation import validate_password
 from .serializers import ProfileUpdateSerializer, ChangePasswordSerializer,UserSerializer
 User = get_user_model()
