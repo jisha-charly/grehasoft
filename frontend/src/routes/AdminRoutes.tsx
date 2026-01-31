f990c94bde59bbaa58d401ff6756f62f35a55224
jisha charly <jisha.charly@gmail.com>
2026-01-31 14:59:09 +0530

    yask management added

--- frontend/src/routes/AdminRoutes.tsx
+++ frontend/src/routes/AdminRoutes.tsx
@@ -23,7 +23,8 @@ const AdminRoutes = () => {
         <Route path="clients" element={<Clients />} />
         <Route path="projects" element={<Projects />} />
         <Route path="projects/:id" element={<ProjectDetails />} />
-        <Route path="taskspage" element={<TasksPage />} />
+        <Route path="tasks" element={<TasksPage />} />
+
         <Route path="settings" element={<Settings />} />
 
         {/* 🔴 ADMIN 404 */}
