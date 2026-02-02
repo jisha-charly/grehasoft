c3243ffa1596fd5f6b8efa7c65ba8b2cf61c9c4a
jisha charly <jisha.charly@gmail.com>
2026-02-02 10:15:07 +0530

    taskform fixed

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -57,6 +57,7 @@ urlpatterns = [
  # ---------------- TASK MANAGEMENT----------------
 
     path("tasks/project/<int:project_id>/", views.list_tasks_by_project),
+     path("task-types/", views.list_task_types),
     path("tasks/", views.create_task),
     path("tasks/<int:task_id>/", views.update_task),
     path("tasks/<int:task_id>/delete/", views.delete_task),
