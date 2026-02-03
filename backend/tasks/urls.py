4969a4969a3ce4d712ca1c394c3a16ca8d14495d
jisha charly <jisha.charly@gmail.com>
2026-02-03 10:51:00 +0530

    tsktypes api fixed

--- backend/tasks/urls.py
+++ backend/tasks/urls.py
@@ -2,15 +2,15 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # TASK TYPES
     path("task-types/", views.list_task_types),
-    path("tasks/", views.create_task),
-    path("tasks/project/<int:project_id>/", views.list_tasks_by_project),
-    path("tasks/<int:task_id>/", views.update_task),
-    path("tasks/<int:task_id>/delete/", views.delete_task),
+    path("task-types/create/", views.create_task_type),
+    path("task-types/<int:pk>/update/", views.update_task_type),
+    path("task-types/<int:pk>/delete/", views.delete_task_type),
+
+    # TASKS
     path("projects/<int:project_id>/tasks/", views.project_tasks),
     path("tasks/assign/", views.assign_task_user),
     path("tasks/unassign/", views.unassign_task_user),
-
-    path("tasks/reorder/", views.update_task_order),
-
+    path("tasks/update-order/", views.update_task_order),
 ]
