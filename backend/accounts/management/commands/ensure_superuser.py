ed0a32df384df40a7a06df4d24f3d107dd26c149
jisha charly <jisha.charly@gmail.com>
2026-02-03 15:24:22 +0530

    add ensure_superuser command for render

--- backend/accounts/management/commands/ensure_superuser.py
+++ backend/accounts/management/commands/ensure_superuser.py
@@ -1,29 +1,32 @@
+import os
 from django.core.management.base import BaseCommand
 from django.contrib.auth import get_user_model
-import os
+
+User = get_user_model()
 
 class Command(BaseCommand):
-    help = "Create superuser if it does not exist"
+    help = "Create superuser if it doesn't exist"
 
     def handle(self, *args, **options):
-        User = get_user_model()
-
         username = os.environ.get("DJANGO_SUPERUSER_USERNAME")
-        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
         email = os.environ.get("DJANGO_SUPERUSER_EMAIL")
+        password = os.environ.get("DJANGO_SUPERUSER_PASSWORD")
 
         if not username or not password:
             self.stdout.write("Superuser env vars not set")
             return
 
         if User.objects.filter(username=username).exists():
-            self.stdout.write("Superuser already exists")
+            user = User.objects.get(username=username)
+            user.is_staff = True
+            user.is_superuser = True
+            user.save()
+            self.stdout.write("Superuser already exists, permissions ensured")
             return
 
         User.objects.create_superuser(
             username=username,
-            password=password,
             email=email,
+            password=password
         )
-
-        self.stdout.write("Superuser created successfully")
+        self.stdout.write("Superuser created")
